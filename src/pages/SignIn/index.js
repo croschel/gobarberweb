8ebbedaae427779532700f7a273c1b7e3c1b7580
croschel <croschel000@gmail.com>
2020-03-17 11:52:54 -0300

    GoBarberWeb with Redux and some styles on auth

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Input, Form } from '@rocketseat/unform';
+import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>Sign In</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Coloque um email válido')
+      .required('Digite um email'),
+    password: Yup.string().required('Digite uma senha'),
+  });
+  return (
+    <>
+      <img src={logo} alt="GoBarberLogo" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu email" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+        <button type="submit">Entrar</button>
+        <Link to="/register">Criar conta gratuitamente</Link>
+      </Form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
+import logo from '~/assets/logo.svg';
 
-// import { Container } from './styles';
+export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
 
-export default function SignUp() {
-  return <div />;
+  const schema = Yup.object().shape({
+    name: Yup.string().required('Digite um nome'),
+    email: Yup.string()
+      .email('Coloque um email válido')
+      .required('Digite um email'),
+    password: Yup.string()
+      .min(6, 'Mínimo 6 caracteres')
+      .required('Digite uma senha'),
+  });
+
+  return (
+    <>
+      <img src={logo} alt="GoBarberLogo" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu email" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+        <button type="submit">Entrar</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
